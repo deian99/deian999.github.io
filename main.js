a8263c0fee915604af6fd4cd4afcf928d9594e5b
deian99 <100875529+deian99@users.noreply.github.com>
2022-07-05 14:57:04 +0300

    Update main.js

--- main.js
+++ main.js
@@ -58,11 +58,12 @@ function searchContact (list, numeCautat) {
 //searchContact(contactList, 'Maria')
 
 class JobOpCard {
-    constructor(title, description, link) {
+    constructor(title, description, link, cardType, image) {
         this.title = '<div id="jobTitle">' + title + '</div>';
         this.description = '<div id="jobDesc">' + description + '</div>';
-        this.image = '<img id="jobImage" alt="image" src=""' + '</img>';
-        this.link = '<div id="jobLink">' + '<a id="jobLink" href="link">' + 'View Posting' + '</a>' + '</div>';
+        this.image = '<img id="jobImage" alt="image"' + ' src="' + image + '"' + '</img>';
+        this.link = '<div id="jobLink">' + '<a id="jobLink" href="#">' + 'View Posting' + '</a>' + '</div>';
+        this.type = cardType;
     }
     string() {
         return this.title + '<br>' + this.description + '<br>' + this.image + '<br>' + this.link
@@ -73,38 +74,54 @@ let listaTitluri = [], listaDescriptions = [], listaImagini = [], listaLink = []
 
 listaTitluri = ["Data Entry Operator", "React Developer", "java Developer", "php Developer", "iOS Developer", "android Developer"]
 listaDescriptions = ["Entry, Fulltime, Remote", "Intermediate, Senior", "Intermediate, Senior", "Senior", "Senior", "Intermediate, Senior"]
+listaImagini = ["/Data-Entry-Operator.jpg", "/React-Thumbnail.jpg", "/Java-Thumbnail.jpg", "/php-Thumbnail.jpg", "/iOS-Thumbnail-1.jpg", "/Android-Thumbnail.jpg",]
 
 
-function populateJobsArray (listaTitlu, listaDesc, listaJobOpCard, Class, link) {
+function populateJobsArray (listaTitlu, listaDesc, listaJobOpCard, Class, link, cardType, image) {
     for (let i = 0; i < 6; i++) {
-        listaJobOpCard.push(new Class(listaTitlu[i], listaDesc[i], link))
+        listaJobOpCard.push(new Class(listaTitlu[i], listaDesc[i], link, cardType, image[i]))
     }
     console.log(listaJobOpCard)
 }
-populateJobsArray(listaTitluri, listaDescriptions, jobOpCards, JobOpCard, "link")
+populateJobsArray(listaTitluri, listaDescriptions, jobOpCards, JobOpCard, "#",1, listaImagini)
 
 
 
-function populateCards (list, mainTitle, mainDesc) {
+function populateCards (list, mainTitle, mainDesc, cardType) {
 
     let wrapper = document.getElementById("wrapper")
     let div = document.createElement("div")
     wrapper.innerHTML = '<div id=wrapperTitle>'+ mainTitle + '</div>'
     wrapper.innerHTML+= '<div id=wrapperDescription>'+ mainDesc + '</div>'
     for (let i = 0; i < list.length; i++) {
-        div.innerHTML+= `<div id="card">` + list[i].string() + `</div>`
-        div.style.display = "grid"
-        div.style.gridTemplateColumns = "repeat(auto-fit, 250px)"
-        div.style.gridRowGap = "20%";
-        div.style.gridColumnGap = "10%"
-        div.style.padding = "5px"
-        wrapper.appendChild(div)
-        div.style.justifyItems = "center"
+        div.innerHTML+= `<div id="card">` + list[i].string() + `</div>`;
+        div.style.display = "grid";
+        div.style.gridTemplateColumns = "repeat(auto-fit, 250px)";
+        div.style.gridRowGap = "10%";
+        div.style.gridColumnGap = "10%";
+        div.style.padding = "5px";
+        wrapper.appendChild(div);
+        div.style.justifyItems = "center";
+    }
+    if (cardType === 1) {
+        for (let i = 0; i < list.length; i++) {
+            document.getElementById("card").id = "card" + cardType
+
+            document.body.style.background = "none"
+        }
+    }
+    else {
+        for (let i = 0; i < list.length; i++) {
+        document.getElementById("card").id = "card" + cardType
+            document.getElementById("wrapperTitle").style.color = "white"
+            document.body.style.background = "url(https://home.ametras.ro/wp-content/uploads/2022/02/ametras-gradient.png)"
+        }
+        div.style.color = "white"
     }
 }
 
 
-let listaTitluriJos = [], listaDescriptionsJos = [], jobOpCardsJos = [];
+let listaTitluriJos = [], listaDescriptionsJos = [], jobOpCardsJos = [], listaImaginiJos = [];
 listaTitluriJos = ["Shape your path forward", "A fair share for everyone", "Security and support", "Office perks and goodies", "Remote work", "Health and wellness"]
 listaDescriptionsJos = ["We provide career development opportunities for all our employees, regardless of their level of experience. There's always something new to learn.",
 "Communication is key, contribution is valued and rewarded. We take responsibility for the quality of the work we deliver and the impact we have on others.",
@@ -112,15 +129,7 @@ listaDescriptionsJos = ["We provide career development opportunities for all our
 "Swimming pool, ping-pong and football table, beer dispenser, Christmas gifts and the occasional parties included.",
 "We allow full flexibility in choosing your work setup—be it from home, from the office, or even from abroad.",
 "We cover a range of medical benefits such as a free ophthalmologic consultation.",]
+listaImaginiJos = ["/Forward.svg", "/Fair-share.svg", "/Security.svg", "/Offices-2.svg", "/Remote.svg", "/Healthcare.svg"]
 
-populateJobsArray(listaTitluriJos, listaDescriptionsJos, jobOpCardsJos, JobOpCard, "link")
+populateJobsArray(listaTitluriJos, listaDescriptionsJos, jobOpCardsJos, JobOpCard, "#", 2, listaImaginiJos)
 //populateCards(jobOpCardsJos, "A career that keeps on giving", "text" )
-
-
-
-
-
-
-
-
-
