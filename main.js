1b5e6e7c72ef1883b0fc2a73e9a13d82a1579e94
deian99 <100875529+deian99@users.noreply.github.com>
2022-07-14 11:38:50 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,135 +1,403 @@
-
-
-let title = document.getElementById("title")
-
-class Contact {
-    constructor(nume, telefon, adresa) {
-        this.nume = `<div id="contactInfo">` + "Nume: " + nume + `</div>`;
-        this.telefon = `<div id="contactInfo">` + "NrTelefon: " + telefon + `</div>`;
-        this.adresa = `<div id="contactInfo">` + "Adresa: " + adresa  + `</div>`;
-    }
-    string() {
-
-        return this.nume + `<br>` + this.telefon + `<br>` + this.adresa;
-    };
+function populateJobsArray(listaJoburi, listaJobOpCard, Class) {
+  for (let i = 0; i < listaJoburi.length; i++) {
+    listaJobOpCard.push(new Class(listaJoburi[i]));
+    console.log(listaJobOpCard);
+  }
+  console.log(listaJobOpCard);
 }
+populateJobsArray(listaJoburi, jobOpCards, JobOpCard);
 
+populateJobsArray(listaJoburiJos, jobOpCardsJos, JobOpCard);
 
-let contactList = [];
-let listaNume = ["Alex", "Deian", "Costel", "Alin", "Cornel", "Marius", "Marius", "Alexandra", "Maria", "Roxi", "Angela", "Dani"]
-let listaNrTel = ["12345", "35421", "54321", "32412", "21343", "53412", "32425", "32415", "23145", "33345", "53321", "22351"]
-let listaAdresa = ["Str A nr 1", "Str B nr 2", "Str C nr 3", "Str D nr 4", "Str E nr 5", "Str F nr 6", "Str G nr 7", "Str H nr 8", "Str I nr 9", "Str J nr 10", "Str K nr 10", "Str L nr 11"]
+function populateCards(list, mainTitle, mainDesc, cardType, clickArray) {
+  let container = document.getElementById("container");
+  let wrapper = document.createElement("div");
+  let background = document.createElement("div");
+  let div = document.createElement("div");
 
-for(let i=0;i<12;i++)
-{
-    contactList.push(new Contact(listaNume[i], listaNrTel[i], listaAdresa[i]))
-}
-console.log(contactList)
-let options = ""
-for (let i=0; i < contactList.length;i++) {
+  background.id = "section" + cardType;
+  wrapper.id = "wrapper";
+  container.appendChild(background);
+  background.appendChild(wrapper);
 
-}
-let nume1 = document.getElementById("nume")
-let h3 = document.createElement("h3")
-let br = document.createElement("br")
+  wrapper.innerHTML +=
+    "<div id=wrapperTitle" +
+    cardType +
+    ">" +
+    mainTitle +
+    "</div>" +
+    "<div id=wrapperDescription" +
+    cardType +
+    ">" +
+    mainDesc +
+    "</div>";
 
+  for (let i = 0; i < list.length; i++) {
+    div.innerHTML += `<div class="card" >` + list[i].string() + `</div>`;
+    div.style.display = "grid";
+    div.style.gridTemplateColumns = "repeat(auto-fit, 375px)";
+    div.style.marginTop = "20px";
+    wrapper.appendChild(div);
+    div.style.justifyItems = "center";
+  }
 
-
-
-
-function searchContact (list, numeCautat) {
-    let finalResult = [];
-    console.log(nume1.value)
-    let resultList = list.filter(element => element.nume === numeCautat)
-    console.log(resultList)
-    for (let i = 0; i < resultList.length; i++) {
-        finalResult.push(resultList[i].string())
-        h3.innerHTML = finalResult;
-        h3.appendChild(br)
-        title.appendChild(h3)
-        console.log(finalResult)
-    }
-    for (let i = 0; i < finalResult.length; i++) {
-
-        h3.appendChild(br)
-    }
-    }
-
-//searchContact(contactList, 'Maria')
-
-class JobOpCard {
-    constructor(title, description, link, cardType, image) {
-        this.title = '<div id="jobTitle">' + title + '</div>';
-        this.description = '<div id="jobDesc">' + description + '</div>';
-        this.image = '<img id="jobImage" alt="image"' + ' src="' + image + '"' + '</img>';
-        this.link = '<div id="jobLink">' + '<a id="jobLink" href="#">' + 'View Posting' + '</a>' + '</div>';
-        this.type = cardType;
-    }
-    string() {
-        return this.title + '<br>' + this.description + '<br>' + this.image + '<br>' + this.link
-    }
-}
-
-let listaTitluri = [], listaDescriptions = [], listaImagini = [], listaLink = [], jobOpCards =[];
-
-listaTitluri = ["Data Entry Operator", "React Developer", "java Developer", "php Developer", "iOS Developer", "android Developer"]
-listaDescriptions = ["Entry, Fulltime, Remote", "Intermediate, Senior", "Intermediate, Senior", "Senior", "Senior", "Intermediate, Senior"]
-listaImagini = ["/Data-Entry-Operator.jpg", "/React-Thumbnail.jpg", "/Java-Thumbnail.jpg", "/php-Thumbnail.jpg", "/iOS-Thumbnail-1.jpg", "/Android-Thumbnail.jpg",]
-
-
-function populateJobsArray (listaTitlu, listaDesc, listaJobOpCard, Class, link, cardType, image) {
-    for (let i = 0; i < 6; i++) {
-        listaJobOpCard.push(new Class(listaTitlu[i], listaDesc[i], link, cardType, image[i]))
-    }
-    console.log(listaJobOpCard)
-}
-populateJobsArray(listaTitluri, listaDescriptions, jobOpCards, JobOpCard, "#",1, listaImagini)
-
-
-
-function populateCards (list, mainTitle, mainDesc, cardType) {
-
-    let wrapper = document.getElementById("wrapper")
-    let div = document.createElement("div")
-    wrapper.innerHTML = '<div id=wrapperTitle>'+ mainTitle + '</div>'
-    wrapper.innerHTML+= '<div id=wrapperDescription>'+ mainDesc + '</div>'
+  if (cardType === 1) {
     for (let i = 0; i < list.length; i++) {
-        div.innerHTML+= `<div id="card">` + list[i].string() + `</div>`;
-        div.style.display = "grid";
-        div.style.gridTemplateColumns = "repeat(auto-fit, 250px)";
-        div.style.gridRowGap = "10%";
-        div.style.gridColumnGap = "10%";
-        div.style.padding = "5px";
-        wrapper.appendChild(div);
-        div.style.justifyItems = "center";
+      let jobTitle = document.querySelector(".jobTitle");
+      let jobDesc = document.querySelector(".jobDesc");
+      let card = document.querySelector(".card");
+      let link = document.querySelector(".jobLink");
+      card.setAttribute("class", "card" + cardType);
+      jobTitle.setAttribute("class", "jobTitle" + cardType);
+      jobDesc.setAttribute("class", "jobDesc" + cardType);
+      link.setAttribute("class", "jobLink" + cardType);
+      card.setAttribute("onclick", clickArray[i]);
     }
-    if (cardType === 1) {
-        for (let i = 0; i < list.length; i++) {
-            document.getElementById("card").id = "card" + cardType
-
-            document.body.style.background = "none"
-        }
+  } else {
+    for (let i = 0; i < list.length; i++) {
+      let jobTitle = document.querySelector(".jobTitle");
+      let jobDesc = document.querySelector(".jobDesc");
+      let card = document.querySelector(".card");
+      let link = document.querySelector(".jobLink");
+      card.setAttribute("class", "card" + cardType);
+      jobTitle.setAttribute("class", "jobTitle" + cardType);
+      jobDesc.setAttribute("class", "jobDesc" + cardType);
+      link.setAttribute("class", "jobLink" + cardType);
+      background.style.background =
+        "url(https://home.ametras.ro/wp-content/uploads/2022/02/ametras-gradient.png)";
     }
-    else {
-        for (let i = 0; i < list.length; i++) {
-        document.getElementById("card").id = "card" + cardType
-            document.getElementById("wrapperTitle").style.color = "white"
-            document.body.style.background = "url(https://home.ametras.ro/wp-content/uploads/2022/02/ametras-gradient.png)"
-        }
-        div.style.color = "white"
-    }
+  }
 }
 
+function addData(
+  titlesList,
+  locationList,
+  expList,
+  projectDescription,
+  introductionList,
+  reqTitles,
+  reqList,
+  additionalTitle,
+  additionalList,
+  weOfferTitle,
+  weOfferList
+) {
+  let jobPages1 = [];
+  for (let i = 0; i < 6; i++) {
+    let newJobPage = new jobPage(
+      titlesList[i],
+      locationList[i],
+      expList[i],
+      projectDescription[i],
+      introductionList[i],
+      reqTitles[i],
+      reqList[i],
+      additionalTitle[i],
+      additionalList[i],
+      weOfferTitle[i],
+      weOfferList[i]
+    );
+    jobPages1.push(newJobPage);
+    // console.log(newJobPage)
+    // console.log(jobPages1)
+  }
+  return jobPages1;
+}
+let jobPageList = addData(
+  jobTitles,
+  jobLocation,
+  jobReq,
+  projectDescription,
+  introductionList,
+  jobReqTitle,
+  jobReqList,
+  jobAdditionalTitle,
+  jobAdditional,
+  jobWeOfferTitles,
+  jobWeOffer
+);
 
-let listaTitluriJos = [], listaDescriptionsJos = [], jobOpCardsJos = [], listaImaginiJos = [];
-listaTitluriJos = ["Shape your path forward", "A fair share for everyone", "Security and support", "Office perks and goodies", "Remote work", "Health and wellness"]
-listaDescriptionsJos = ["We provide career development opportunities for all our employees, regardless of their level of experience. There's always something new to learn.",
-"Communication is key, contribution is valued and rewarded. We take responsibility for the quality of the work we deliver and the impact we have on others.",
-"We're in this together for the long run. Many of our employees and clients have been together with us from day one.",
-"Swimming pool, ping-pong and football table, beer dispenser, Christmas gifts and the occasional parties included.",
-"We allow full flexibility in choosing your work setup—be it from home, from the office, or even from abroad.",
-"We cover a range of medical benefits such as a free ophthalmologic consultation.",]
-listaImaginiJos = ["/Forward.svg", "/Fair-share.svg", "/Security.svg", "/Offices-2.svg", "/Remote.svg", "/Healthcare.svg"]
+console.log(jobPageList);
 
-populateJobsArray(listaTitluriJos, listaDescriptionsJos, jobOpCardsJos, JobOpCard, "#", 2, listaImaginiJos)
-//populateCards(jobOpCardsJos, "A career that keeps on giving", "text" )
+console.log(jobPageList);
+
+function showJobDetails(newJobPage) {
+  let container = document.getElementById("container");
+  let body = document.getElementById("body")
+  body.innerHTML = ""
+  body.appendChild(container)
+  container.innerHTML = "";
+  let content = document.createElement("div");
+  content.setAttribute("class", "content");
+
+  let mainTitle = document.createElement("p");
+  mainTitle.setAttribute("class", "main-title");
+  mainTitle.innerHTML = newJobPage.jobTitles;
+  let applyButton = document.createElement("button");
+  applyButton.innerHTML = "APPLY NOW";
+  applyButton.setAttribute("class", "apply-button");
+
+  let jobTitle = document.createElement("p");
+  jobTitle.setAttribute("class", "job-title");
+  jobTitle.innerHTML = newJobPage.jobTitles;
+  let location = document.createElement("p");
+  location.setAttribute("class", "location");
+  location.innerHTML = newJobPage.jobLocation;
+  let experience = document.createElement("p");
+  experience.setAttribute("class", "job-req");
+  experience.innerHTML = newJobPage.jobExperience;
+
+  let introduction = document.createElement("div");
+  introduction.setAttribute("class", "introduction");
+  let ametras = document.createElement("strong");
+  ametras.innerHTML = "Ametras Service ";
+  let compIntrod = document.createElement("span");
+  compIntrod.innerHTML = newJobPage.compIntroduction;
+
+  let projectDescription = document.createElement("div");
+  let projectDescriptionText = document.createElement("span");
+  let u = document.createElement("u");
+  u.innerHTML = "Project description:";
+  projectDescriptionText.innerHTML = newJobPage.projectDescription;
+  projectDescription.appendChild(u);
+  if (newJobPage.projectDescription === "") {
+    projectDescription.removeChild(u);
+  }
+  projectDescription.appendChild(projectDescriptionText);
+
+  let reqTitle = document.createElement("div");
+  reqTitle.innerHTML = "<strong>" + newJobPage.requireTitle + "</strong>";
+  let ul = document.createElement("ul");
+  newJobPage.requireList.forEach(
+    (element) => (ul.innerHTML += "<li>" + element + "</li>")
+  );
+
+  let additionalTitle = document.createElement("div");
+  additionalTitle.innerHTML =
+    "<strong>" + newJobPage.additionalTitle + "</strong>";
+  let additionalList = document.createElement("div");
+  let additionalUl = document.createElement("ul");
+  newJobPage.additionalList.forEach(
+    (element) => (additionalUl.innerHTML += "<li>" + element + "</li>")
+  );
+  additionalList.appendChild(additionalUl);
+
+  let offerTitle = document.createElement("div");
+  offerTitle.innerHTML = "<strong>" + newJobPage.jobOfferTitle + "</strong>";
+  let offerList = document.createElement("div");
+  let offerUl = document.createElement("ul");
+  newJobPage.jobOfferList.forEach(
+    (element) => (offerUl.innerHTML += "<li>" + element + "</li>")
+  );
+  offerList.appendChild(offerUl);
+  let apply = document.createElement("h4");
+  apply.innerHTML = "Apply Now!";
+
+  let br = document.createElement("br");
+
+  reqTitle.style.marginTop = "40px";
+  additionalTitle.style.marginTop = "40px";
+  additionalList.style.marginTop = "40px";
+  offerTitle.style.marginTop = "40px";
+  offerList.style.marginTop = "40px";
+
+  let form = document.createElement("form");
+  let nameLabel = document.createElement("label");
+  let mailLabel = document.createElement("label");
+  let subjectLabel = document.createElement("label");
+  let socialLabel = document.createElement("label");
+  let nameInput = document.createElement("input");
+  let mailInput = document.createElement("input");
+  let subjectInput = document.createElement("input");
+  let socialInput = document.createElement("input");
+
+  form.setAttribute("action", "");
+
+  nameLabel.setAttribute("for", "name");
+  nameLabel.innerHTML = "Your name" + "<br />";
+  nameLabel.appendChild(nameInput);
+  nameLabel.appendChild(document.createElement("br"));
+
+  mailLabel.setAttribute("for", "email");
+  mailLabel.innerHTML = "Your email" + "<br />";
+  mailLabel.appendChild(mailInput);
+  mailLabel.appendChild(document.createElement("br"));
+
+  subjectLabel.setAttribute("for", "subject");
+  subjectLabel.innerHTML = "Subject" + "<br />";
+  subjectLabel.appendChild(subjectInput);
+  subjectLabel.appendChild(document.createElement("br"));
+
+  socialLabel.setAttribute("for", "social");
+  socialLabel.innerHTML = "Your LinkedIn Account link" + "<br />";
+  socialLabel.appendChild(socialInput);
+  socialLabel.appendChild(document.createElement("br"));
+
+  nameInput.setAttribute("type", "text");
+  nameInput.setAttribute("name", "name");
+  nameInput.setAttribute("id", "name");
+
+  mailInput.setAttribute("type", "text");
+  mailInput.setAttribute("name", "email");
+  mailInput.setAttribute("id", "email");
+
+  subjectInput.setAttribute("type", "text");
+  subjectInput.setAttribute("name", "subject");
+  subjectInput.setAttribute("id", "subject");
+
+  socialInput.setAttribute("type", "text");
+  socialInput.setAttribute("name", "social");
+  socialInput.setAttribute("id", "social");
+
+  let submitButton = document.createElement("button");
+  submitButton.innerHTML = "SUBMIT";
+  submitButton.style.marginBottom = "50px";
+  submitButton.setAttribute("class", "submit-button");
+
+  let backButton = document.createElement("button");
+  backButton.innerHTML = "BACK";
+  backButton.setAttribute("class", "submit-button");
+  backButton.style.cursor = "pointer";
+  backButton.onclick = goBack;
+
+  container.appendChild(content);
+  content.appendChild(mainTitle);
+  content.appendChild(location);
+  content.appendChild(jobTitle);
+  content.appendChild(experience);
+  content.appendChild(applyButton);
+  content.appendChild(introduction);
+  content.appendChild(ametras);
+  content.appendChild(compIntrod);
+  content.appendChild(projectDescription);
+  content.appendChild(reqTitle);
+  content.appendChild(ul);
+  content.appendChild(additionalTitle);
+  content.appendChild(additionalList);
+  content.appendChild(offerTitle);
+  content.appendChild(offerList);
+  content.appendChild(apply);
+  content.appendChild(form);
+  content.appendChild(submitButton);
+
+  form.appendChild(nameLabel);
+  form.appendChild(br);
+  form.appendChild(mailLabel);
+  form.appendChild(br);
+  form.appendChild(subjectLabel);
+  form.appendChild(socialLabel);
+  content.appendChild(backButton);
+  content.appendChild(br);
+
+
+}
+
+function goBack() {
+  let container = document.getElementById("container");
+  container.innerHTML = "";
+  showCarrerPage();
+
+}
+
+function showMainTitle(mainTitle, subTitle, footerTitle) {
+  let header = document.createElement("div");
+  header.setAttribute("class", "header");
+  let body = document.getElementById("body");
+
+  header.innerHTML = '<div class="mainTitle">' + mainTitle + "</div>";
+  header.innerHTML += '<div class="subTitle">' + subTitle + "</div>";
+  body.innerHTML +=
+    '<div class="footer">' +
+    '<div class="footer-title">' +
+    footerTitle +
+    "</div>" +
+    '<button class="footer-button">' +
+    "START TODAY" +
+    "</button>" +
+    "</div>";
+  body.insertBefore(header, body.firstChild);
+}
+
+function showCarrerPage() {
+  showMainTitle(mainPageTitle, mainPageSubtitle, footerTitle);
+
+  populateCards(
+    jobOpCards,
+    `Carrer opportunities`,
+    `We are always looking for ways to expand our collective expertise. Here are some of the opportunities available in our team.`,
+    1,
+    onclickArray
+  );
+
+  populateCards(
+    jobOpCardsJos,
+    `A career that keeps on giving`,
+    ``,
+    2,
+    jobPageList
+  );
+  createSlider(slides)
+}
+
+showCarrerPage();
+
+
+
+
+function createSlider (slides) {
+  let body = document.getElementById("body");
+  let h4 = document.createElement("h4")
+  h4.id = "stories"
+  h4.innerHTML = "Our Stories"
+  body.appendChild(h4)
+  //let slider = document.getElementById("inner-slider")
+  let slider = document.createElement("div")
+  let innerSlider = document.createElement("div")
+  slider.setAttribute("class", "slider")
+  innerSlider.id = "inner-slider"
+  slider.appendChild(innerSlider)
+  body.appendChild(slider)
+  let footer = document.querySelector(".footer")
+
+  //let container = document.querySelector(".slider")
+
+  body.insertBefore(slider, footer)
+  body.insertBefore(h4, slider)
+  // slider.innerHTML += '<button class="previous" onclick="plusSlides(-1)">' + 'PREVIOUS QUOTE'+ '</button>'
+  // slider.innerHTML += '<button class="next" onclick="plusSlides(1)">' + 'NEXT QUOTE'+ '</button>'
+  for (let i = 0; i < slides.length; i++) {
+    innerSlider.innerHTML += '<div class="slide">' + '<q class="slide-text">' +slides[i].quote + '</q>'+ '<p class="slide-name">' + slides[i].name + '</p>' + '<p class="slide-company">' + slides[i].company + '</p>' + '</div>'
+  }
+  let slide = document.querySelector(".slide")
+  let pressed = false;
+  let startx;
+  let x;
+
+  slider.addEventListener('mousedown', (e) => {
+    pressed = true;
+    startx = e.offsetX - innerSlider.offsetLeft;
+    slider.style.cursor = "grabbing"
+  })
+
+  slider.addEventListener('mouseenter', () => {
+    slider.style.cursor = "grab";
+  })
+
+  slider.addEventListener('mouseup', ()=> {
+    slider.style.cursor = "grab"
+  })
+
+  window.addEventListener('mouseup', () => {
+    pressed = false;
+  })
+
+  slider.addEventListener('mousemove', (e) => {
+    if(!pressed) return
+    e.preventDefault()
+
+    x = e.offsetX
+    innerSlider.style.left = `${x - startx}px`;
+    slide.style.display = "block";
+  })
+
+}
